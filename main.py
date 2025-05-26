69e1e6da4591fbee53443284d70735b56b5bcbc6
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-26 16:53:22 -0300

    updated changes

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import chat
 
 app = FastAPI(
     title="Promtior RAG Chatbot",
-    description="Chatbot que responde usando el PDF de Promtior con LangChain y GPT4All.",
+    description="Chatbot que responde usando el PDF de Promtior con LangChain y Together AI.",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(chat.router, prefix="/chat", tags=["Chat"])
